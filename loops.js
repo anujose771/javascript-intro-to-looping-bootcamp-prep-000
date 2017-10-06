5337c08d8f58bf46142f2a9ce4ec84bcb5327337
blue shadow <anujose1989@gmail.com>
2017-10-06 21:28:33 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -8,7 +8,7 @@
 
  function forLoop(array){
    for(let i = 0 ; i < 25; i++){
-     array[i] = `I am ${i} strange loop`+ (`${i} == 0 ? '.' : 's.'`);
+     array[i] = `I am ${i} strange loop`+ (`${i} === 1 ? '.' : 's.'`);
    }
    return array;
  }
